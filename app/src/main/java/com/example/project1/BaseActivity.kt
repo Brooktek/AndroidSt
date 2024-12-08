c45fc14d58109d59c3a87565da41d593ee16aa71
Brook Tekle (Opshadow) <126861167+Brooktek@users.noreply.github.com>
2024-12-08 22:01:39 +0300

    just started

---
+++ app/src/main/java/com/example/project1/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.example.project1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import android.view.WindowManager
+import androidx.core.view.WindowInsetsCompat
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    }
+}
