a6de627646650de680a2ebe8ab93bf803f6770e4
Opshadow <brookteklebrhan123@gmail.com>
2024-12-19 11:54:04 +0300

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.project1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.project1"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
